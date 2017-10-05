86d2b5ee93311dd268ae531ae93106d105444d9b
fendy1996 <fendy1996fast@hotmaill.com>
2017-10-05 11:26:59 +0800

    105

---
+++ client/main.js
@@ -0,0 +1,8 @@
+var hellomessage = "Hello Wourld!";
+console.log(hellomessage);
+hellomessage ="yoooo";
+console.log(hellomessage);
+
+var fruit ="apple", applenum ="3";
+console.log(fruit);
+console.log(apple num);
